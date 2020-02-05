8369cbadc3334435caa90f06e1f677ac28af2f58
GowthamRaj K <gowthamjerry1509@gmail.com>
2020-02-05 16:13:57 +0530

    updated

--- src/main/java/payment/WalletAPI.java
+++ src/main/java/payment/WalletAPI.java
@@ -1,20 +1,19 @@
-//package payment;
-//import citipe.servicelayer.*;
-//
-//public class WalletAPI {
-//	
-//	private static final Long COMPANY_MOBILE_NO = 6789012340L; //use your registered wallet mobileno
-//	public static boolean pay(Long mobileNo , int pin, int amount) {
-//		UserService userService = new UserService();
-//		boolean status ;
-//		try {
-//			status = userService.walletTransfer(mobileNo, COMPANY_MOBILE_NO, pin, amount);
-//			System.out.println(status);
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//			status = false;
-//		}
-//        return status;
-//	}
-//	
-//}
+package payment;
+
+public class WalletAPI {
+
+	private static final Long COMPANY_MOBILE_NO = 6789012340L; // use your registered wallet mobileno
+
+	public static boolean pay(Long mobileNo, int pin, int amount) {
+
+		boolean status = false;
+		/*
+		 * try { UserService userService = new UserService(); status =
+		 * userService.walletTransfer(mobileNo, COMPANY_MOBILE_NO, pin, amount);
+		 * System.out.println(status); } catch (Exception e) { e.printStackTrace();
+		 * status = false; }
+		 */
+		return status;
+	}
+
+}
